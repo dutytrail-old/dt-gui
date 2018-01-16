73575a848eb58b9c68df3afeb86a9fae5f1366d8
joan.tolos <joan.tolos@clarivate.com>
2018-01-16 16:51:38 +0100

    Eureka service added

--- src/app/services/duty.service.ts
+++ src/app/services/duty.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Eureka } from 'eureka-js-client';
 
 @Injectable()
 export class DutyService {
 
   constructor() { }
 
+  getClient(): void {
+    return new Eureka({
+      instance: {
+        app: 'dtgui',
+        hostName: 'localhost',
+        port: 8080,
+        vipAddress: 'com.dutytrail',
+        dataCenterInfo: {
+          name: 'Heroku',
+        },
+      },
+      eureka: {
+        host: 'localhost',
+        port: 8761,
+      },
+    });
+  }
 }
